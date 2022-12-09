c5467fb985196630137ee25b6475314ffbc991f4
Aleksvtop <vazilino@gmail.com>
2022-12-09 17:48:40 +0300

    Add logic for counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Button from "./Button";
+import Display from "./Display";
+
+
 
 function App() {
+    const maxValue = 5
+    const [num, setNum] = useState<number>(0)
+    const addHandler = () => {
+        if (num !== maxValue) {
+            setNum(num + 1)
+        }
+    }
+    const resetHandler = () => {
+        setNum(0)
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="Wrapper">
+            <div className={"Display"}>
+                <Display value={num} maxValue={maxValue}/>
+            </div>
+            <div className="Buttons">
+                <Button disabled={num===maxValue} className="Button" name={'Increment'} callBack={addHandler}/>
+                <Button disabled={num===0} className="Button" name={'Reset'} callBack={resetHandler}/>
+            </div>
+        </div>
     </div>
   );
 }
